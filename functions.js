15dacf6d01f77286efecaa35d2a1d331d39d3b25
lehtu <lehtu05@gmail.com>
2015-09-04 23:52:25 +0300

    First working demo

---
+++ functions.js
@@ -0,0 +1,34 @@
+function draw(ctx) {
+	var canvas = $("canvas");
+	canvas.width=1012;//horizontal resolution (?) - increase for better looking text
+	canvas.height=440;//vertical resolution (?) - increase for better looking text
+	canvas.style.width=506;//actual width of canvas
+	canvas.style.height=220;//actual height of canvas
+
+	ctx.rect(0,00,1012,440);
+	ctx.fillStyle="#272822";
+	ctx.fill();
+
+	var fontSize = 44;
+	var canvas = document.getElementById('canvas');
+	var x = canvas.width / 2;
+	var y = canvas.height / 2 - fontSize / 2;   
+	var fillText = "git grep 'objectName' -- '*.php'";
+
+	ctx.textBaseline = "top";
+	ctx.font = fontSize+"px Menlo";
+
+	ctx.textAlign = 'center';
+
+	ctx.fillStyle = "#f1f1f1";
+	ctx.fillText(fillText, x, y);
+}
+
+function downloadCanvas(link, canvasId, filename) {
+	link.href = document.getElementById(canvasId).toDataURL();
+	link.download = filename;
+}
+
+document.getElementById('download').addEventListener('click', function() {
+	downloadCanvas(this, 'canvas', 'test.png');
+}, false);
